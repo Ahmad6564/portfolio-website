b53deeb14cd680f216c449f9a197d4c2aa337137
Ahmad6564 <ahmadkhalid6564@gmail.com>
2025-11-09 22:52:22 -0500

    URL updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,7 @@ const Footer: React.FC = () => {
                                     <a href="https://github.com/Ahmad6564" className="hover:underline hover:text-gray-800 dark:hover:text-gray-200 transition-colors">GitHub</a>
                                 </li>
                                 <li className="mb-2">
-                                    <a href="https://www.linkedin.com/in/ahmad-khalid-846830201/" className="hover:underline hover:text-gray-800 dark:hover:text-gray-200 transition-colors">LinkedIn</a>
+                                    <a href="https://www.linkedin.com/in/muhammad-ahmad-846830201/" className="hover:underline hover:text-gray-800 dark:hover:text-gray-200 transition-colors">LinkedIn</a>
                                 </li>
                                 <li>
                                     <a href="https://medium.com/@ahmadkhalid6564" className="hover:underline hover:text-gray-800 dark:hover:text-gray-200 transition-colors">Medium</a>
@@ -68,7 +68,7 @@ const Footer: React.FC = () => {
                             </svg>
                             <span className="sr-only">GitHub account</span>
                         </a>
-                        <a href="https://www.linkedin.com/in/ahmad-khalid-846830201/" className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 transition-colors">
+                        <a href="https://www.linkedin.com/in/muhammad-ahmad-846830201/" className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 transition-colors">
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fillRule="evenodd" d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" clipRule="evenodd" />
                             </svg>
